87075e420e2c89875765efa2fb3d151c24c3ba4b
Ziyuan Zhao <ninjiacoder@gmail.com>
2019-06-26 19:55:29 +0800

    <feat> finish save query

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from flask import Flask, render_template, request
 import pymysql
 import datetime
@@ -10,11 +11,11 @@ cf = configparser.ConfigParser()
 cf.read('db.conf')
 
 @app.route('/')
-def hello_world():
-    dashboard_select_result = dashboard_sql_select_execute("select * from record")
+def home():
+    dashboard_select_result = dashboard_sql_execute("select * from record")
     result = []
     for record in dashboard_select_result:
-        behavior_select_result = behavior_sql_select_execute(record[1])
+        behavior_select_result = behavior_sql_execute(record[1])
         x_data = []
         y_data = []
         for row in behavior_select_result:
@@ -36,26 +37,38 @@ def new():
         x_title = request.form['x_title']
         y_title = request.form['y_title']
         sql = request.form['sql']
-        x_data = []
-        y_data = []
-        behavior_select_result = behavior_sql_select_execute(sql)
-        for row in behavior_select_result:
-            if (type(row[0]) == datetime.date):
-                x_data.append(row[0].strftime("%Y-%m-%d"))
-                y_data.append(row[1])
-            else:
-                x_data.append(row[0])
-                y_data.append(row[1]) 
-        result = {
-            "title": title,
-            "x_title": x_title,
-            "y_title": y_title,
-            "x_data": x_data,
-            "y_data": y_data
-        }
+        print("insert into record (title, xtitle, ytitle, query) values ('%s', '%s', '%s', '%s')" % (title, x_title, y_title, pymysql.escape_string(sql)))
+        dashboard_sql_execute("insert into record (title, xtitle, ytitle, query) values ('%s', '%s', '%s', '%s')" % (title, x_title, y_title, pymysql.escape_string(sql))) 
+        
+        result = {"status": "success"}
         return json.dumps(result)
 
-def dashboard_sql_select_execute(sql):
+@app.route('/quicklook', methods=["POST"])
+def quicklook():
+    title = request.form['title']
+    x_title = request.form['x_title']
+    y_title = request.form['y_title']
+    sql = request.form['sql']
+    x_data = []
+    y_data = []
+    behavior_select_result = behavior_sql_execute(sql)
+    for row in behavior_select_result:
+        if (type(row[0]) == datetime.date):
+            x_data.append(row[0].strftime("%Y-%m-%d"))
+            y_data.append(row[1])
+        else:
+            x_data.append(row[0])
+            y_data.append(row[1]) 
+    result = {
+        "title": title,
+        "x_title": x_title,
+        "y_title": y_title,
+        "x_data": x_data,
+        "y_data": y_data
+    }
+    return json.dumps(result) 
+
+def dashboard_sql_execute(sql):
     connection = pymysql.connect(
         host = cf.get('a2os_dashboard_db', 'a2os_dashboard_db_host'),
         port = cf.getint('a2os_dashboard_db', 'a2os_dashboard_db_port'),
@@ -66,9 +79,13 @@ def dashboard_sql_select_execute(sql):
     )
     cursor = connection.cursor()
     cursor.execute(sql)
-    return cursor.fetchall()
+    connection.commit()
+    select_result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return select_result
 
-def behavior_sql_select_execute(sql):
+def behavior_sql_execute(sql):
     connection = pymysql.connect(
         host=cf.get('behavior_db', 'behavior_db_host'),
         port=cf.getint('behavior_db', 'behavior_db_port'),
@@ -79,4 +96,8 @@ def behavior_sql_select_execute(sql):
     )
     cursor = connection.cursor()
     cursor.execute(sql)
-    return cursor.fetchall()
+    connection.commit()
+    select_result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return select_result
